532078a858d4512a1dd2d4970eba60e9c15266c1
NicolasdeBourgoing <nicolas.debourgoing@gmail.com>
2026-07-19 19:10:46 +0200

    Création de la logique player pour sous-charger le main (inputs, update et init)

---
+++ player.py
@@ -0,0 +1,22 @@
+from ursina import *
+
+class Player(Entity):
+    def __init__(self, **kwargs):
+        super().__init__(
+            model='cube', 
+            color=color.orange, 
+            scale_y=2, 
+            **kwargs
+        )
+        self.player_model = Entity(model='player', parent=self)
+        self.controls = {'left': 'q', 'right': 'd'} # Configuration AZERTY
+
+    def update(self):
+        # Logique de déplacement propre au joueur
+        self.x -= held_keys[self.controls['left']] * time.dt
+        self.x += held_keys[self.controls['right']] * time.dt
+
+    def input(self, key):
+        if key == 'space':
+            self.y += 1
+            invoke(setattr, self, 'y', self.y - 1, delay=0.25)

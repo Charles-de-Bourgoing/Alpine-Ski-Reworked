c9c332f4b20e01ac64cd10515930b4a0d151c785
Charles <charles.debourgoing@gmail.com>
2026-07-22 21:18:12 +0200

    modèle skieur

--- player.py
+++ player.py
@@ -4,9 +4,10 @@ from physics import SkiPhysics
 class Player(Entity):
     def __init__(self, **kwargs):
         super().__init__(
-            model='cube',
+            model='models/skier_low_poly_character',
             scale=(0.8, 1.8, 0.5), # Proportion plus proche d'un skieur
-            color=color.orange,
+            #texture='models/skier_low_poly_character',
+            texture='models/gradient',
             origin_y=-0.5,
             collider='box',
             **kwargs
